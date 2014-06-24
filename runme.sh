ee8528a72f0dacb6c1ca30ea9ad1e655132b85e1
Gianpaolo Macario <gianpaolo_macario@mentor.com>
2014-06-24 11:17:22 +0200

    Fix checksum file forma to work on Linux
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

--- runme.sh
+++ runme.sh
@@ -8,6 +8,8 @@ ARCHIVE_URL=http://sourceforge.net/projects/alcatel/files/${ARCHIVE_FILENAME}/do
 ARCHIVE_MD5=b5b406b30e054aef76bf13b6937e5c7c
 ARCHIVE_SHA1=76b05a23549aab7e0b9d58833d59d6d527f202e9
 
+tmpfile=runme-$$.tmp
+
 if [ ! -f ${ARCHIVE_FILENAME} ]; then
     echo "INFO: Downloading ${ARCHIVE_FILENAME}... "
     wget -O ${ARCHIVE_FILENAME} ${ARCHIVE_URL}
@@ -15,15 +17,19 @@ fi
 
 if [ "${ARCHIVE_MD5}" != "" ]; then
     echo "INFO: Checking MD5... "
-    echo ${ARCHIVE_MD5} ${ARCHIVE_FILENAME} | md5sum -c -
+    echo "${ARCHIVE_MD5}  ${ARCHIVE_FILENAME}" >$tmpfile
+    md5sum -c $tmpfile
 fi
 
 if [ "${ARCHIVE_SHA1}" != "" ]; then
     echo "INFO: Checking SHA1... "
-    echo ${ARCHIVE_SHA1} ${ARCHIVE_FILENAME} | sha1sum -c -
+    echo "${ARCHIVE_SHA1}  ${ARCHIVE_FILENAME}" >$tmpfile
+    sha1sum -c $tmpfile
 fi
 
 echo "INFO: Extracting file contents..."
 tar xpvf ${ARCHIVE_FILENAME}
+
+rm -f $tmpfile
 
 # EOF
